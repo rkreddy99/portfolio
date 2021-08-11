7804c34eabc9d096c2896ffd516416ed77d65b99
Rama Krishna Reddy <orkr123@gmail.com>
2021-08-11 21:26:57 +0530

    refactor: create folders for project and blog images

--- projects.js
+++ projects.js
@@ -2,7 +2,7 @@ let projectDetails = [
   {
       title: "Do you know me?",
       description: "A fun CLI app based on NodeJS that can quiz my friends on how well do they know me.",
-      image: "./images/know-me.png",
+      image: "./images/project/know-me.png",
       date: "July 2020",
       site: "https://replit.com/@Rama-KrishnaKri/KnowMe?embed=1&output=1",
       source: "https://github.com/rkreddy99/know-me"
@@ -10,7 +10,7 @@ let projectDetails = [
   {
       title: "Are you a potter-head?",
       description: "Let's checkout how well do you know about Harry Potter series by J. K. Rowling.",
-      image: "./images/potter-head.png",
+      image: "./images/project/potter-head.png",
       date: "July 2020",
       site: "https://replit.com/@Rama-KrishnaKri/PotterHead?embed=1&output=1",
       source: "https://github.com/rkreddy99/movies-quiz"
@@ -18,7 +18,7 @@ let projectDetails = [
   {
       title: "Minion Speak",
       description: "Did you know that all the gibberish that minions speak is an actual language. Use this app to translate text from English to the Minion language or Banana laguage.",
-      image: "./images/minion-language.png",
+      image: "./images/project/minion-language.png",
       date: "July 2020",
       site: "https://minion-translator-m6.netlify.app/",
       source: "https://github.com/rkreddy99/minion-translator"
@@ -26,7 +26,7 @@ let projectDetails = [
   {
       title: "Speak Dothraki",
       description: "Have you heard of Dothraki language and wanted to speak it? Well here is an app that can translate text from English to Dothraki.",
-      image: "./images/speak-dothraki.png",
+      image: "./images/project/speak-dothraki.png",
       date: "July 2020",
       site: "https://dothraki-translator-m7.netlify.app/",
       source: "https://github.com/rkreddy99/dothraki-translator"
@@ -34,7 +34,7 @@ let projectDetails = [
   {
       title: "Emoji Interpreter",
       description: "This a fun app that can tell you meaning of animals and nature emojis.",
-      image: "./images/know-emoji.png",
+      image: "./images/project/know-emoji.png",
       date: "July 2020",
       site: "https://know-ur-emoji-m8.netlify.app/",
       source: "https://github.com/rkreddy99/emoji-detector"
@@ -42,7 +42,7 @@ let projectDetails = [
   {
       title: "Good Movies",
       description: "Do you love to watch movies? You would find some good recommendations.",
-      image: "./images/good-movies.png",
+      image: "./images/project/good-movies.png",
       date: "Aug 2020",
       site: "https://movie-recommender-m09.netlify.app/",
       source: "https://github.com/rkreddy99/genre-recommender"
@@ -50,7 +50,7 @@ let projectDetails = [
   {
       title: "Cash Register",
       description: "This is an useful app that can tell the amount that a cashier has to return to the customer upon bill payment.",
-      image: "./images/cash-register.png",
+      image: "./images/project/cash-register.png",
       date: "July 2020",
       site: "https://cash-registry-m10.netlify.app/",
       source: "https://github.com/rkreddy99/cash-register"
@@ -58,7 +58,7 @@ let projectDetails = [
   {
       title: "Is your birthday lucky?",
       description: "Find out whether your birthdate is lucky or not.",
-      image: "./images/lucky-birthday.png",
+      image: "./images/project/lucky-birthday.png",
       date: "July 2020",
       site: "https://lucky-birthday-m11.netlify.app/",
       source: "https://github.com/rkreddy99/lucky-birthday"
@@ -66,14 +66,14 @@ let projectDetails = [
   {
       title: "Fun with Triangles",
       description: "Find out properties of triangles and play a quiz on triangles.",
-      image: "./images/fun-triangles.png",
+      image: "./images/project/fun-triangles.png",
       date: "Aug 2020",
       site: "https://fun-with-triangles-m12.netlify.app/",
       source: "https://github.com/rkreddy99/fun-with-triangles"
   },
   {
       title: "Palindromic Birthday",
-      image: "./images/palindrome-birthday.png",
+      image: "./images/project/palindrome-birthday.png",
       description: "Use this app to check whether if your birthdate forms a palindrome.",
       site: "https://palindrome-birthday-m13.netlify.app/",
       source: "https://github.com/rkreddy99/palindrome-birthday",
@@ -82,7 +82,7 @@ let projectDetails = [
   {
       title: "Stock PnL Calculator",
       description: "Find out how much profit/loss have you made on a stock.",
-      image: "./images/stock-price.png",
+      image: "./images/project/stock-price.png",
       date: "Aug 2020",
       site: "https://stock-price-calculator-m14.netlify.app/",
       source: "https://github.com/rkreddy99/stock-price-calculator"
